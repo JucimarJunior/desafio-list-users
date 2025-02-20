6e99e9915c44eabc696378e836f7756e75816b87
Jucimar Junior <jucimar.vinente@hotmail.com>
2025-02-20 00:54:58 -0300

    Organizando em mÃpacotes diferentes e removendo a view da main activity

---
+++ app/src/main/java/com/picpay/desafio/android/ui/ContactFragment.kt
@@ -0,0 +1,89 @@
+package com.picpay.desafio.android.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ProgressBar
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.picpay.desafio.android.R
+import com.picpay.desafio.android.model.User
+import com.picpay.desafio.android.UserListAdapter
+import com.picpay.desafio.android.service.UserService
+import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ContactFragment : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var progressBar: ProgressBar
+    private lateinit var adapter: UserListAdapter
+
+    private val url = "https://609a908e0f5a13001721b74e.mockapi.io/picpay/api/"
+
+    private val gson: Gson by lazy { GsonBuilder().create() }
+
+    private val okHttp: OkHttpClient by lazy {
+        OkHttpClient.Builder()
+            .build()
+    }
+
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(url)
+            .client(okHttp)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+    }
+
+    private val service: UserService by lazy {
+        retrofit.create(UserService::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_contact, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerView = view.findViewById(R.id.recyclerView)
+        progressBar = view.findViewById(R.id.user_list_progress_bar)
+
+        adapter = UserListAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        progressBar.visibility = View.VISIBLE
+
+        service.getUsers()
+            .enqueue(object : Callback<List<User>> {
+                override fun onFailure(call: Call<List<User>>, t: Throwable) {
+//                    val message = getString(R.string.error)
+
+                    progressBar.visibility = View.GONE
+                    recyclerView.visibility = View.GONE
+
+//                    Toast.makeText(this@ContactFragment, message, Toast.LENGTH_SHORT)
+//                        .show()
+                }
+
+                override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) {
+                    progressBar.visibility = View.GONE
+
+                    adapter.users = response.body()!!
+                }
+            })
+
+    }
+}
