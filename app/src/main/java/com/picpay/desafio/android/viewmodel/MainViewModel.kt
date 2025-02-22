fb6f7d7da2876f462895cbeedab8346b15a57cea
Jucimar Junior <jucimar.vinente@hotmail.com>
2025-02-22 13:09:34 -0300

    Ajustando a classe MainViewModel

--- app/src/main/java/com/picpay/desafio/android/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/picpay/desafio/android/viewmodel/MainViewModel.kt
@@ -1,7 +1,6 @@
 package com.picpay.desafio.android.viewmodel
 
 import android.util.Log
-import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.picpay.desafio.android.model.User
